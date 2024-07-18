f213e560772d6ae72dc9faa89091af2c3091582f
Black Mann <caranzocrisa@gmail.com>
2024-07-18 11:13:24 +0800

    lead commit

--- admin/config/dbcon.php
+++ admin/config/dbcon.php
@@ -1,13 +1,14 @@
 <?php 
-// $host = "127.0.0.1";
-// $username = "mcclrc";
-// $password = "mcclrc";
-// $database = "mcclrc";
 
  $host = "localhost";
- $username = "u510162695_mcclrc";
- $password = "1Mcclrc_pass";
- $database = "u510162695_mcclrc";
+ $username = "root";
+ $password = "";
+ $database = "mcclrc";
+
+ //$host = "localhost";
+ //$username = "u510162695_mcclrc";
+ //$password = "1Mcclrc_pass";
+ //$database = "u510162695_mcclrc";
 
 $con = mysqli_connect("$host", "$username", "$password", "$database");
 
