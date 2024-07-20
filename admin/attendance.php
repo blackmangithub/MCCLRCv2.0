aa38aec1cd0c92825bba504ffae1caac79cee7a6
Black Mann <caranzocrisa@gmail.com>
2024-07-20 14:51:02 +0800

    lead commit

--- admin/attendance.php
+++ admin/attendance.php
@@ -16,6 +16,48 @@ include('./includes/sidebar.php');
      padding: 5px 10px;
      margin: 10px 3px 10px 0;
 }
+.sname, .dated, .tname{
+    display: none;
+}
+
+@media print {
+    body *{
+     visibility: hidden;
+    }
+    #myDataTabele, .sname, .dated, .tname, #myDataTable *{
+     visibility: visible;
+    } 
+    .sname{
+    display: block;
+    position: fixed;
+            left: 0px;
+            top: 30px;
+            font-weight: bold;
+     }
+     .dated{
+    display: block;
+    position: fixed;
+            top: 10px;
+            right: 0px;
+            font-size: 15px;
+     }
+     .data_table{
+        position: fixed;
+        left: 0px;
+        top: 100px;
+        right: 0px;
+    }
+    .tname {
+     display: block;
+            position: fixed;
+            left: 0;
+            top: 130px;
+            right: 0;
+            font-weight: bold;
+            text-align: center;
+            font-size: 20px;
+    }
+}
 </style>
 <main id="main" class="main">
      <div class="pagetitle" data-aos="fade-down">
@@ -39,11 +81,19 @@ include('./includes/sidebar.php');
                               <div class="card recent-sales overflow-auto  border-3 border-top border-info">
 
                                    <div class="card-body">
-                                        <div class="row d-flex justify-content-around align-items-center mt-2">
-                                             <h5 class="card-title col-12 col-md-3 px-3 text-center">
-                                                  Students Attendance
-                                             </h5>
-                                             <form action="" method="POST" class="col-12 col-md-6 d-flex ">
+                                        <div class="row d-flex justify-content-end align-items-center mt-2">
+                                        <div class="text-start">
+                                            <button onclick="exportToPDF()" class="btn btn-danger pdf-button">
+                                                <i class="bi bi-file-earmark-pdf-fill"></i> <b>Export to PDF</b>
+                                            </button>
+                                            <button onclick="exportToExcel()" class="btn btn-success excel-button">
+                                                <i class="bi bi-file-earmark-excel-fill"></i> <b>Export to Excel</b>
+                                            </button>
+                                            <button onclick="window.print()" class="btn btn-primary print-button">
+                                                <i class="bi bi-printer-fill"></i> <b>Print</b>
+                                            </button>
+                                        </div>
+                                             <form action="" method="POST" class="col-12 col-md-5 d-flex ">
 
                                                   <?php date_default_timezone_set('Asia/Manila'); ?>
                                                   <div class="form-group form-group-sm">
@@ -72,8 +122,12 @@ include('./includes/sidebar.php');
                                                   <div class="col-12">
 
                                                        <div class="data_table">
-                                                            <table id="example"
+                                                            <table id="myDataTable"
                                                                  class="table table-striped table-bordered">
+                                                                 <h5 class="dated">Date: <?php echo date('F d, Y'); ?></h5>
+                                                <h1 class="sname">MCC Learning Resource Center</h1>
+                                                <h2 class="tname">Attendance List</h2>
+                                                <br>
                                                                  <thead>
                                                                       <tr>
                                                                       <th>Date</th>
@@ -155,9 +209,45 @@ include('./includes/sidebar.php');
           </div>
      </section>
 </main>
-<script>
 
-</script>
+<script>
+        async function exportToPDF() {
+            const { jsPDF } = window.jspdf;
+            const doc = new jsPDF();
+
+            doc.autoTable({
+                html: '#myDataTable',
+                styles: { fontSize: 8 },
+                headStyles: { fillColor: [0, 0, 0] },
+                startY: 20
+            });
+
+            doc.save('attendance_report.pdf');
+        }
+
+        function exportToExcel() {
+            var wb = XLSX.utils.book_new();
+            var ws_data = [
+                ['Date', 'Time in', 'Full Name', 'Program', 'Time out']
+            ];
+
+            var table = document.querySelector('#myDataTable tbody');
+            var rows = table.querySelectorAll('tr');
+
+            rows.forEach(function(row) {
+                var cells = row.querySelectorAll('td');
+                var row_data = [];
+                cells.forEach(function(cell) {
+                    row_data.push(cell.innerText);
+                });
+                ws_data.push(row_data);
+            });
+
+            var ws = XLSX.utils.aoa_to_sheet(ws_data);
+            XLSX.utils.book_append_sheet(wb, ws, "Sheet1");
+            XLSX.writeFile(wb, "attendance_report.xlsx");
+        }
+    </script>
 
 
 <?php 
