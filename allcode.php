640d6fe9b4c317b0ee1abdd38fad9f89c96cf93a
Black Mann <caranzocrisa@gmail.com>
2024-07-20 20:48:26 +0800

    lead commit

--- allcode.php
+++ allcode.php
@@ -1,149 +1,103 @@
 <?php
 session_start();
 
-
 include('admin/config/dbcon.php');
 
-if(isset($_POST['change_password']))
-{
+if (isset($_POST['password_update'])) {
      $current_password = mysqli_real_escape_string($con, $_POST['current_password']);
      $newpassword = mysqli_real_escape_string($con, $_POST['newpassword']);
      $renewpassword = mysqli_real_escape_string($con, $_POST['renewpassword']);
-
-     $validate_query = "SELECT * FROM user WHERE password = md5('$current_password')";
-     $validate_query_run = mysqli_query($con, $validate_query);
-
-     if(mysqli_num_rows($validate_query_run) > 0)
-     {
-          if($newpassword == $renewpassword)
-          {
-               $change_pass = "UPDATE `user` SET password= md5('$newpassword'),  confirm_password=md5('$renewpassword')";
-               $change_pass_run = mysqli_query($con, $change_pass);
-
-               if($change_pass_run)
-               {
-                    $_SESSION['message_success'] = '<small>Password updated successfully</small>';
-                    header("Location: myprofile.php");
-                    exit(0);
-               }
-               else
-               {
-                    $_SESSION['message_error'] = 'Password not updated';
-                    header("Location: myprofile.php");
-                    exit(0);
-               }
-          }
-          else
-          {
-               $_SESSION['message_error'] = '<small>Password and confirm password not match</small>';
-               header("Location: myprofile.php");
-               exit(0);
-          }
+ 
+     // Validate the current password for user or faculty
+     $current_password_hashed = md5($current_password);
+     
+     $validate_user_query = "SELECT * FROM user WHERE password = '$current_password_hashed'";
+     $validate_faculty_query = "SELECT * FROM faculty WHERE password = '$current_password_hashed'";
+ 
+     $validate_user_query_run = mysqli_query($con, $validate_user_query);
+     $validate_faculty_query_run = mysqli_query($con, $validate_faculty_query);
+ 
+     if (!$validate_user_query_run || !$validate_faculty_query_run) {
+         die('Query failed: ' . mysqli_error($con));
      }
-     else
-     {
-          $_SESSION['message_error'] = 'Current password not match';
-          header("Location: myprofile.php");
-          exit(0);
+ 
+     if (mysqli_num_rows($validate_user_query_run) > 0 || mysqli_num_rows($validate_faculty_query_run) > 0) {
+         if ($newpassword == $renewpassword) {
+             $newpassword_hashed = md5($newpassword);
+             if (mysqli_num_rows($validate_user_query_run) > 0) {
+                 // Update password for user
+                 $change_pass_query = "UPDATE `user` SET password = '$newpassword_hashed' WHERE password = '$current_password_hashed'";
+             } else {
+                 // Update password for faculty
+                 $change_pass_query = "UPDATE `faculty` SET password = '$newpassword_hashed' WHERE password = '$current_password_hashed'";
+             }
+             $change_pass_run = mysqli_query($con, $change_pass_query);
+ 
+             if (!$change_pass_run) {
+                 $_SESSION['message_error'] = 'Password not updated: ' . mysqli_error($con);
+                 header("Location: myprofile.php");
+                 exit(0);
+             }
+ 
+             $_SESSION['message_success'] = '<small>Password updated successfully</small>';
+             header("Location: myprofile.php");
+             exit(0);
+         } else {
+             $_SESSION['message_error'] = '<small>Password and confirm password do not match</small>';
+             header("Location: myprofile.php");
+             exit(0);
+         }
+     } else {
+         $_SESSION['message_error'] = 'Current password does not match';
+         header("Location: myprofile.php");
+         exit(0);
      }
-}
-
-
-if (isset($_SESSION['auth_stud']['stud_id']))
-{
-     $id_session=$_SESSION['auth_stud']['stud_id'];
-
  }
-                
-          
+ 
 
-
-if(isset($_POST['save_changes']))
-{
-     $firstname = mysqli_real_escape_string($con, $_POST['firstname']);
-     $middlename = mysqli_real_escape_string($con, $_POST['middlename']);
-     $lastname = mysqli_real_escape_string($con, $_POST['lastname']);
-     $address = mysqli_real_escape_string($con, $_POST['address']);
-     $phone = mysqli_real_escape_string($con, $_POST['phone']);
-     $email = mysqli_real_escape_string($con, $_POST['email']);
-
-
-    //  $old_admin_filename = $_POST['old_admin_image'];
-
-    //  $admin_image = $_FILES['admin_image']['name'];
-
-    //  $update_admin_filename = "";
-
-    //  if($admin_image != NULL)
-    //  {
-           // Rename the Image
-    //        $admin_extension = pathinfo($admin_image, PATHINFO_EXTENSION);
-    //        $admin_filename = time().'.'.$admin_extension;
-
-    //        $update_admin_filename =  $admin_filename;
-    //  }
-    //  else
-    //  {
-    //       $update_admin_filename = $old_admin_filename;
-    //  }
-
-     $query = "UPDATE `user` SET  firstname='$firstname', middlename='$middlename', lastname='$lastname', address='$address', cell_no='$phone', email='$email' WHERE user_id ='$id_session' ";
-     $query_run = mysqli_query($con, $query);
-
-     if($query_run)
-     {
-          // if($admin_image != NULL)
-          // {
-          //      if(file_exists('../uploads/admin_profile/'.$old_admin_filename))
-          //      {
-          //           unlink("../uploads/admin_profile/".$old_admin_filename);
-          //      }
-          // }
-          // move_uploaded_file($_FILES['admin_image']['tmp_name'], '../uploads/admin_profile/'.$admin_filename);
-
-          $_SESSION['message_success'] = 'Updated Successfully';
-          header("Location: myprofile.php");
-          exit(0);
-     }
-     else
-     {
-          $_SESSION['message_error'] = 'Not Updated';
-          header("Location: myprofile.php");
-          exit(0);
-     }
+if (isset($_SESSION['auth_stud']['stud_id'])) {
+    $id_session = $_SESSION['auth_stud']['stud_id'];
+} elseif (isset($_SESSION['auth_faculty']['faculty_id'])) {
+    $id_session = $_SESSION['auth_faculty']['faculty_id'];
 }
 
+if (isset($_POST['save_changes'])) {
+    $firstname = mysqli_real_escape_string($con, $_POST['firstname']);
+    $middlename = mysqli_real_escape_string($con, $_POST['middlename']);
+    $lastname = mysqli_real_escape_string($con, $_POST['lastname']);
+    $address = mysqli_real_escape_string($con, $_POST['address']);
+    $phone = mysqli_real_escape_string($con, $_POST['phone']);
+    $email = mysqli_real_escape_string($con, $_POST['email']);
 
+    // Update profile for user or faculty
+    if (isset($_SESSION['auth_stud']['stud_id'])) {
+        // Update user profile
+        $query = "UPDATE `user` SET firstname='$firstname', middlename='$middlename', lastname='$lastname', address='$address', cell_no='$phone', email='$email' WHERE user_id ='$id_session'";
+    } else {
+        // Update faculty profile
+        $query = "UPDATE `faculty` SET firstname='$firstname', middlename='$middlename', lastname='$lastname', address='$address', cell_no='$phone', email='$email' WHERE faculty_id ='$id_session'";
+    }
+    $query_run = mysqli_query($con, $query);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Logout
-if(isset($_POST['logout_btn']))
-{
-// session_destroy();
-unset($_SESSION['auth']);
-unset($_SESSION['auth_role']);
-unset($_SESSION['auth_stud']);
-
-
-$_SESSION['message_success'] ="Logout Successfully";
-header("Location: home.php");
-exit(0);
+    if ($query_run) {
+        $_SESSION['message_success'] = 'Updated Successfully';
+        header("Location: myprofile.php");
+        exit(0);
+    } else {
+        $_SESSION['message_error'] = 'Not Updated';
+        header("Location: myprofile.php");
+        exit(0);
+    }
 }
 
+if (isset($_POST['logout_btn'])) {
+    unset($_SESSION['auth']);
+    unset($_SESSION['auth_role']);
+    unset($_SESSION['auth_stud']);
+    unset($_SESSION['auth_faculty']);
 
+    $_SESSION['message_success'] = "Logout Successfully";
+    header("Location: home.php");
+    exit(0);
+}
 ?>
