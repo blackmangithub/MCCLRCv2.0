b9bf65726180763d537a8d700f90f213be10d336
Black Mann <caranzocrisa@gmail.com>
2024-07-20 21:37:22 +0800

    lead commit

--- password-reset-code.php
+++ password-reset-code.php
@@ -82,7 +82,7 @@ function send_password_reset($get_name, $get_email, $token)
             <div class='content'>
             <p>Hello,</p>
             <p>We received a request to reset your password. Click the button below to reset it:</p>
-            <p><a style='color: white;' href='http://localhost/MCCLRCV2.0/password-change.php?token=$token&email=$get_email' class='button'>Reset Password</a></p>
+            <p><a style='color: white;' href='http://mcc-lrc.com/password-change.php?token=$token&email=$get_email' class='button'>Reset Password</a></p>
             <p>If you did not request a password reset, please ignore this email.</p>
             
             </div>
