e561338e53ae2881af2bdf76f5f7c911685357dd
Black Mann <caranzocrisa@gmail.com>
2024-07-21 22:55:23 +0800

    lead commit

--- admin/config/dbcon.php
+++ admin/config/dbcon.php
@@ -1,23 +1,20 @@
 <?php 
 
- $host = "localhost";
- $username = "u510162695_mcclrc";
- $password = "1Mcclrc_pass";
- $database = "u510162695_mcclrc";
-//  $username = "root";
-//  $password = "";
-//  $database = "mcclrc";
+$host = "localhost";
+$username = "u510162695_mcclrc";
+$password = "1Mcclrc_pass";
+$database = "u510162695_mcclrc";
 
-$con = mysqli_connect("$host", "$username", "$password", "$database");
+// Uncomment these lines if using local development setup
+// $username = "root";
+// $password = "";
+// $database = "mcclrc";
 
-if(!$con)
-{
-  echo "Connection failed";
-  die();
+$con = mysqli_connect($host, $username, $password, $database);
+
+if (!$con) {
+  die("Connection failed: " . mysqli_connect_error());
+} else {
+  echo "Connected Successfully";
 }
-// else
-// {
-//   echo "Connected Successfully";
-//   die();
-// }
 ?>
