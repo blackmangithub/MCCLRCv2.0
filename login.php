9113a0ce67a351ad843f36a705d46f5637786d55
Black Mann <caranzocrisa@gmail.com>
2024-07-23 17:25:31 +0800

    lead commit

--- login.php
+++ login.php
@@ -9,8 +9,33 @@ session_start();
 //}
 
 include('./admin/config/dbcon.php');
-include('./includes/header.php');
 ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+     <meta charset="UTF-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <link rel="icon" href="./assets/img/mcc-logo.png">
+     <title>MCC Learning Resource Center</title>
+
+     <!-- Alertify JS link -->
+     <link rel="stylesheet" href="assets/css/alertify.min.css" />
+     <link rel="stylesheet" href="assets/css/alertify.bootstraptheme.min.css" />
+
+     <!-- Iconscout cdn link -->
+     <link rel="stylesheet" href="assets/css/line.css">
+     <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
+     
+     <!-- Bootstrap CSS -->
+     <link rel="stylesheet" href="assets/css/bootstrap5.min.css" />
+
+     <!-- Bootstrap Icon -->
+     <link rel="stylesheet" href="assets/font/bootstrap-icons.css">
+
+     <!-- Custom CSS Styling -->
+     <link rel="stylesheet" href="assets/css/login.css">
 
      <style>
           .back{
