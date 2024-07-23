9ad0c2088b0fc92dbda20bd62e385dab19d65813
Black Mann <caranzocrisa@gmail.com>
2024-07-23 16:20:02 +0800

    lead commit

--- includes/navbar.php
+++ includes/navbar.php
@@ -16,8 +16,8 @@ include('admin/config/dbcon.php');
                <ul class="navbar-nav ms-auto mb-2 mb-lg-0 nav nav-pills nav-justified">
                     <?php if(isset($_SESSION['auth_stud'])) :?>
                     <li class="nav-item">
-                         <a class="nav-link text-white <?=$page == 'index.php' || $page == 'book_details.php' ? 'active': '' ?> fw-semibold"
-                              href="index.php">Home</a>
+                         <a class="nav-link text-white <?=$page == 'ebook.php' || $page == 'book_details.php' ? 'active': '' ?> fw-semibold"
+                              href="ebook.php">Home</a>
                     </li>
                     <li class="nav-item">
                          <a class="nav-link text-white <?=$page == 'ebook.php' ? 'active': '' ?> fw-semibold"
