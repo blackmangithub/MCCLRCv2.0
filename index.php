958d595dcc2c40b786311f269c7edb6a429373ad
Black Mann <caranzocrisa@gmail.com>
2024-07-24 20:18:48 +0800

    lead commit

--- index.php
+++ index.php
@@ -24,6 +24,20 @@ if ($_SESSION['auth_role'] != "student" && $_SESSION['auth_role'] != "faculty" &
             <div class="mx-2">
             <img src="assets/img/mcc-logo.png" class="d-sm-none d-md-block me-4" style="height: 100px; width: 100px;" alt="MCC Logo">
             </div>
+            <div class="col-8 mt-2">
+              <center>
+                <h3 class="fw-semibold">Madridejos Community College</h3>
+                <h4 class="fw-semibold">Learning Resource Center</h4>
+              </center>
+              <form class=" " method="GET">
+                <div class="d-flex">
+                  <div class="input-group mb-3 me-6">
+                    <input type="text" name="search" value="<?php if(isset($_GET['search'])) { echo htmlspecialchars($_GET['search']); } ?>" class="form-control" placeholder="Type here to search" required>
+                    <button class="btn btn-primary px-md-5 px-sm-1">Search</button>
+                  </div>
+                </div>
+              </form>
+            </div>
           </div>
         </div>
       </div>
