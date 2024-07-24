66ca5acd1cddc0e74185f235acf96fd506ad8b6a
Black Mann <caranzocrisa@gmail.com>
2024-07-25 07:35:10 +0800

    lead commit

--- index.php
+++ index.php
@@ -55,8 +55,7 @@ if ($_SESSION['auth_role'] != "student" && $_SESSION['auth_role'] != "faculty" &
             if (isset($_GET['search'])) { 
               $filtervalues = mysqli_real_escape_string($con, $_GET['search']);
               $query = "SELECT book.*, COUNT(book.accession_number) AS copy_count,
-                        SUM(CASE WHEN book.status = 'available' THEN 1 ELSE 0 END) AS available_count,
-                        book.status_hold = ''
+                        SUM(CASE WHEN book.status = 'available' THEN 1 ELSE 0 END) AS available_count
                         FROM book 
                         WHERE title LIKE '%$filtervalues%'
                         GROUP BY title
@@ -101,8 +100,7 @@ if ($_SESSION['auth_role'] != "student" && $_SESSION['auth_role'] != "faculty" &
               }
             } else {
               $query = "SELECT book.*, COUNT(book.accession_number) AS copy_count, 
-                        SUM(CASE WHEN book.status = 'available' THEN 1 ELSE 0 END) AS available_count,
-                        book.status_hold = ''
+                        SUM(CASE WHEN book.status = 'available' THEN 1 ELSE 0 END) AS available_count 
                         FROM book 
                         GROUP BY title 
                         ORDER BY title DESC";
