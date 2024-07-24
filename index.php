a7df160b77b51d5c835b2912a169dd31828d2f44
Black Mann <caranzocrisa@gmail.com>
2024-07-24 20:10:29 +0800

    lead commit

--- index.php
+++ index.php
@@ -1,14 +1,13 @@
 <?php 
-ini_set('display_error', '1');
 include('includes/header.php');
 include('includes/navbar.php');
 include('admin/config/dbcon.php');
 
-if (empty($_SESSION['auth'])) {
+if(empty($_SESSION['auth'])){
+//   $_SESSION['message_error'] = "<small>Login your Credentials to Access</small>";
   header('Location: home.php');
   exit(0);
 }
-
 if ($_SESSION['auth_role'] != "student" && $_SESSION['auth_role'] != "faculty" && $_SESSION['auth_role'] != "staff") 
 {
   header("Location:index.php");
