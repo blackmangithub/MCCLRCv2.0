cb16513ad73aee95bf105e04efc43e32490dd324
Black Mann <caranzocrisa@gmail.com>
2024-07-25 07:39:27 +0800

    lead commit

--- index.php
+++ index.php
@@ -100,9 +100,7 @@ if ($_SESSION['auth_role'] != "student" && $_SESSION['auth_role'] != "faculty" &
               }
             } else {
               $query = "SELECT book.*, COUNT(book.accession_number) AS copy_count, 
-                        SUM(CASE WHEN book.status = 'available' THEN 1 ELSE 0 END) AS available_count,
-                        book.status_hold = ' '
-                        FROM book 
+                        SUM(CASE WHEN book.status = 'available' THEN 1 ELSE 0 END) AS available_count                        FROM book 
                         GROUP BY title 
                         ORDER BY title DESC";
               $query_run = mysqli_query($con, $query);
