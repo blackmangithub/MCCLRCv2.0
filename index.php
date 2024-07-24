e3ee71c80bdc54c8576bdb5286cba52665e0e2ea
Black Mann <caranzocrisa@gmail.com>
2024-07-25 06:49:00 +0800

    lead commit

--- index.php
+++ index.php
@@ -15,3 +15,26 @@ if ($_SESSION['auth_role'] != "student" && $_SESSION['auth_role'] != "faculty" &
 }
 ?>
 
+<div class="container">
+  <div class="row">
+    <div class="col-12">
+      <div class="card mt-4" data-aos="fade-up">
+        <div class="card-header">
+          <div class="d-flex align-items-center justify-content-center mt-2">
+            <div class="mx-2">
+              <img src="assets/img/mcc-logo.png" class="d-sm-none d-dm-block me-2"
+              style="height: 100px; width: 100px;" alt="MCC Logo">
+            </div>
+
+            <div class="col-8 mt-2">
+              <center>
+                <h3 class="fw-semibold">Madridejos Community College</h3>
+                <h4 class="fw-semibold">Learning Resource Center</h4>
+              </center>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
