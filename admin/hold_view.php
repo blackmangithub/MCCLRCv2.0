cc4a4376961d6d75b5f8d07facf077b141893f50
Black Mann <caranzocrisa@gmail.com>
2024-07-25 09:00:43 +0800

    lead commit

--- admin/hold_view.php
+++ admin/hold_view.php
@@ -41,6 +41,49 @@ $user_row = $user_result->fetch_assoc();
                     </div>
                     <div class="card-body">
                         <div class="table-responsive mt-3">
+                        <?php 
+include('authentication.php');
+include('includes/header.php'); 
+include('./includes/sidebar.php'); 
+
+$id = $_GET['id'];
+$type = $_GET['type'];
+
+$query = "";
+if($type == "user") {
+    $query = "SELECT * FROM user WHERE user_id = ?";
+} else if($type == "faculty") {
+    $query = "SELECT * FROM faculty WHERE faculty_id = ?";
+}
+
+$user_stmt = $con->prepare($query);
+$user_stmt->bind_param("s", $id);
+$user_stmt->execute();
+$user_result = $user_stmt->get_result();
+$user_row = $user_result->fetch_assoc();
+?>
+
+<main id="main" class="main">
+    <div class="pagetitle">
+        <h1>Hold Books</h1>
+        <nav>
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="index.php">Home</a></li>
+                <li class="breadcrumb-item"><a href="hold_list.php">Hold Books</a></li>
+                <li class="breadcrumb-item active">View Hold Books</li>
+            </ol>
+        </nav>
+    </div>
+    <section class="section">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="card">
+                    <div class="card-header text-bg-primary d-flex fw-semibold justify-content-between">
+                        <h5 class="m-0 text-white fw-semibold">Recent Hold Books</h5>
+                        <a href="hold_list.php" class="btn btn-success">Back</a>
+                    </div>
+                    <div class="card-body">
+                        <div class="table-responsive mt-3">
                             <table id="myDataTable" cellpadding="0" cellspacing="0" border="0"
                                    class="table table-striped table-bordered">
                                 <thead>
@@ -91,6 +134,28 @@ $user_row = $user_result->fetch_assoc();
                                 No Hold Books at this Moment
                             </div>
                             <?php } ?>
+                        </div>
+                    </div>
+                    <div class="card-footer"></div>
+                </div>
+            </div>
+        </div>
+    </section>
+</main>
+
+<?php 
+include('./includes/footer.php');
+include('./includes/script.php');
+include('../message.php');   
+?>
+
+<script>
+var select_box_element = document.querySelector('#select_box');
+dselect(select_box_element, {
+    search: true
+});
+</script>
+
                         </div>
                     </div>
                     <div class="card-footer"></div>
