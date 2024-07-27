0cf83f4e901717b3ea22898b200a649dcfea83f9
Black Mann <caranzocrisa@gmail.com>
2024-07-27 15:39:18 +0800

    lead commit

--- includes/navbar.php
+++ includes/navbar.php
@@ -5,13 +5,13 @@ include('admin/config/dbcon.php');
 <nav class="navbar navbar-expand-lg" style="background: #0096FF;">
      <?php  $page = substr($_SERVER['SCRIPT_NAME'], strrpos($_SERVER['SCRIPT_NAME'], "/")+ 1); ?>
      <div class="container-fluid mx-5">
-          <img src="assets/img/mcc-logo.png" alt="logo" class=" mx-2" height="40px" width="40px" />
-          <a class="navbar-brand text-white fw-bold fs-5" href="#">MCC-LRC</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+     <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                aria-label="Toggle navigation">
                <span class="navbar-toggler-icon"></span>
           </button>
+          <img src="assets/img/mcc-logo.png" alt="logo" class=" mx-2" height="40px" width="40px" />
+          <a class="navbar-brand text-white fw-bold fs-5" href="#">MCC-LRC</a>
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
                <ul class="navbar-nav ms-auto mb-2 mb-lg-0 nav nav-pills nav-justified">
                     <?php if(isset($_SESSION['auth_stud'])) :?>
